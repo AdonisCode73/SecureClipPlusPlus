b43efd0da7fe237a47618803368c36487549dd3e
adoniscode73 <adoniscode73@gmail.com>
2026-03-09 22:27:08 +0000

    Encryption & Decryption via sodium - Password protection too

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,17 +4,56 @@
 #include <list>
 #include <algorithm>
 #include <fstream>
+#include "sodium.h"
 
 using json = nlohmann::json;
 
 std::forward_list<std::wstring> content = {};
+std::string g_password;
 
 // TODO:
 // -> Save clipboard content to disk == DONE
 // -> Load content from disk == DONE
-// -> Encrypt the content with password
+// -> Encrypt the content with password == DONE
 // -> CLI interface to retrieve, clear and access n entry (Later replace with Imgui interface?)
 // -> Keyboard shortcuts
+
+std::string toBase64(const unsigned char* data, int len) {
+	size_t b64len = sodium_base64_encoded_len(len, sodium_base64_VARIANT_ORIGINAL);
+	std::string result(b64len, '\0');
+	sodium_bin2base64(result.data(), b64len, data, len, sodium_base64_VARIANT_ORIGINAL);
+	result.resize(strlen(result.c_str()));
+	return result;
+}
+
+std::vector<unsigned char> fromBase64(const std::string &b64) {
+	std::vector<unsigned char> result(b64.size());
+	size_t binLen;
+	if (sodium_base642bin(result.data(), result.size(), b64.data(), b64.size(), nullptr, &binLen, nullptr, sodium_base64_VARIANT_ORIGINAL) < 0) {
+		throw std::runtime_error("Base 64 decoding failed");
+	}
+
+	result.resize(binLen);
+	return result;
+}
+
+std::string retrievePassword(const std::string& prompt) {
+	std::cout << prompt << std::endl;
+
+	HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+	DWORD mode = 0;
+
+	GetConsoleMode(hStdin, &mode);
+	SetConsoleMode(hStdin, mode & ~ENABLE_ECHO_INPUT);
+
+	std::string password;
+	std::getline(std::cin, password);
+
+	SetConsoleMode(hStdin, mode);
+	std::cout << std::endl;
+
+	return password;
+}
 
 void writeToDisk(const std::forward_list<std::wstring>& content) {
 	std::vector<std::string> vec;
@@ -28,22 +67,81 @@ void writeToDisk(const std::forward_list<std::wstring>& content) {
 			vec.push_back(utf8String);
 		}
 	}
-	json jsonData = vec;
-	std::ofstream outFile("data.json");
+
+	std::string plainText = json(vec).dump();
+
+	unsigned char salt[crypto_pwhash_SALTBYTES];
+	randombytes_buf(salt, sizeof(salt));
+
+	unsigned char nonce[crypto_secretbox_NONCEBYTES];
+	randombytes(nonce, sizeof(nonce));
+
+	unsigned char key[crypto_box_SEEDBYTES];
+	if (crypto_pwhash
+		(key, sizeof(key), g_password.c_str(), g_password.size(), salt,
+			crypto_pwhash_OPSLIMIT_INTERACTIVE, crypto_pwhash_MEMLIMIT_INTERACTIVE,
+			crypto_pwhash_ALG_DEFAULT) != 0) 
+	{
+		std::cerr << "Deriving key failed - probably out of memory" << std::endl;
+		sodium_memzero(key, sizeof(key));
+		return;
+	}
+
+	std::vector<unsigned char> cypherText(crypto_secretbox_MACBYTES + plainText.size());
+
+	crypto_secretbox_easy(cypherText.data(), reinterpret_cast<const unsigned char*>(plainText.c_str()), plainText.size(), nonce, key);
+	sodium_memzero(key, sizeof(key));
+
+
+	json outData = {
+		{"salt", toBase64(salt, sizeof(salt))},
+		{"nonce", toBase64(nonce, sizeof(nonce))},
+		{"cypher", toBase64(cypherText.data(), cypherText.size())}
+	};
+
+
+	std::ofstream outFile("data.enc");
 
 	if (outFile) {
-		outFile << jsonData;
+		outFile << outData;
 	}
 	else {
-		std::cerr << "Failed to write JSON";
+		std::cerr << "Failed to write encrypted data";
 	}
 }
 
-void loadFromDisk(std::forward_list<std::wstring>& content) {
-	std::ifstream inFile("data.json");
+bool loadFromDisk(std::forward_list<std::wstring>& content) {
+	std::ifstream inFile("data.enc");
 
-	json jsonData;
-	inFile >> jsonData;
+	json inputData;
+	inFile >> inputData;
+
+	std::vector<uint8_t> saltVec = fromBase64(inputData["salt"].get<std::string>());
+	std::vector<uint8_t> nonceVec = fromBase64(inputData["nonce"].get<std::string>());
+	std::vector<uint8_t> cypherVec = fromBase64(inputData["cypher"].get<std::string>());
+
+	if (saltVec.size() != crypto_pwhash_SALTBYTES || nonceVec.size() != crypto_secretbox_NONCEBYTES || cypherVec.size() < crypto_secretbox_MACBYTES) {
+		std::cerr << "Corrupted Data - probably" << std::endl;
+		return false;
+	}
+
+	unsigned char key[crypto_box_SEEDBYTES];
+	if (crypto_pwhash(key, sizeof(key), g_password.c_str(), g_password.size(), saltVec.data(),
+		crypto_pwhash_OPSLIMIT_INTERACTIVE, crypto_pwhash_MEMLIMIT_INTERACTIVE, crypto_pwhash_ALG_DEFAULT) < 0) {
+		std::cerr << "Deriving key failed" << std::endl;
+		return false;
+	}
+
+	std::vector<unsigned char> plainText(cypherVec.size() - crypto_secretbox_MACBYTES);
+	if (crypto_secretbox_open_easy(plainText.data(), cypherVec.data(), cypherVec.size(), nonceVec.data(), key) < 0) {
+		std::cerr << "Decryption Failed" << std::endl;
+		return false;
+	}
+
+	sodium_memzero(key, sizeof(key));
+
+	std::string plainTextStr(plainText.begin(), plainText.end());
+	json jsonData = json::parse(plainTextStr);
 
 	// Retrieve UTF8 string and return it back to wide character
 	for (const auto& val : jsonData) {
@@ -55,6 +153,8 @@ void loadFromDisk(std::forward_list<std::wstring>& content) {
 			content.push_front(wideCharString);
 		}
 	}
+
+	return true;
 }
 
 LRESULT CALLBACK hiddenWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
@@ -156,7 +256,25 @@ int main() {
 		clipBoardErrorHandler();
 	}
 
-	loadFromDisk(content);
+	if (sodium_init() < 0) {
+		std::cerr << "Failed to init Sodium" << std::endl;
+		exit(1);
+	}
+
+
+	if (std::ifstream("data.enc")) {
+		
+		g_password = retrievePassword("Enter password to login: ");
+		
+		if (!loadFromDisk(content)) {
+			std::cerr << "Failed to load content from disk - Aborting" << std::endl;
+			exit(1);
+		}
+	}
+	else {
+		g_password = retrievePassword("First time login - Set your password: ");
+	}
+
 	for (auto& val : content) {
 		std::wcout << val << std::endl;
 
